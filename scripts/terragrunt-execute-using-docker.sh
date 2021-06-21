389780ff95cc26a416f57f142557eba6c45bcc95
Silvio Silva <smsilva@gmail.com>
2021-06-21 15:36:42 -0300

    Script atualizado

--- scripts/terragrunt-execute-using-docker.sh
+++ scripts/terragrunt-execute-using-docker.sh
@@ -22,9 +22,17 @@ docker run \
     --user $(id -u):$(id -g) \
     --workdir /data \
     --env GIT_SSH_COMMAND='ssh -i /data/.ssh/id_rsa -o "StrictHostKeyChecking=no"' \
+    --env ARM_CLIENT_ID="${ARM_CLIENT_ID?}" \
+    --env ARM_CLIENT_SECRET="${ARM_CLIENT_SECRET?}" \
+    --env ARM_SUBSCRIPTION_ID="${ARM_SUBSCRIPTION_ID?}" \
+    --env ARM_TENANT_ID="${ARM_TENANT_ID?}" \
+    --env ARM_ACCESS_KEY="${ARM_ACCESS_KEY?}" \
+    --env GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS?}" \
+    --env GOOGLE_PROJECT="${GOOGLE_PROJECT?}" \
+    --env GOOGLE_REGION="${GOOGLE_REGION?}" \
+    --env GOOGLE_ZONE="${GOOGLE_ZONE?}" \
     --volume ${HOME}/.ssh:/data/.ssh \
     --volume $(pwd):/data \
     --volume ~/.azure:/data/.azure \
-    --volume ~/.gitconfig:/data/.gitconfig \
     --volume /tmp/fake_passwd:/etc/passwd \
-    devopsinfra/docker-terragrunt:azure-tf-1.0.0-tg-0.30.7 terragrunt run-all plan --terragrunt-working-dir stacks/sandbox
+    devopsinfra/docker-terragrunt:azure-tf-1.0.0-tg-0.30.7 terragrunt run-all plan --terragrunt-working-dir ./
