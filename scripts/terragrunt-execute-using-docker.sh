6140f3c25f95031e1b69bcbc67f912e3f5de054d
Silvio Silva <smsilva@gmail.com>
2021-06-23 09:30:26 -0300

    Script

--- scripts/terragrunt-execute-using-docker.sh
+++ scripts/terragrunt-execute-using-docker.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 echo "git-user:x:$(id -u):$(id -g):Git User:/tmp:/bin/bash" > /tmp/fake_passwd
 
-docker run \
-    --rm \
-    --tty \
-    --interactive \
-    --user $(id -u):$(id -g) \
-    --workdir /data \
-    --env GIT_SSH_COMMAND='ssh -i /data/.ssh/id_rsa -o "StrictHostKeyChecking=no"' \
-    --volume ${HOME}/.ssh:/data/.ssh \
-    --volume $(pwd):/data \
-    --volume ~/.azure:/data/.azure \
-    --volume ~/.gitconfig:/data/.gitconfig \
-    --volume /tmp/fake_passwd:/etc/passwd \
-    devopsinfra/docker-terragrunt:azure-tf-1.0.0-tg-0.30.7 bash
+TERRAFORM_VERSION="1.0.0"
+TERRAGRUNT_VERSION="0.30.7"
 
 docker run \
     --rm \
@@ -35,4 +24,5 @@ docker run \
     --volume $(pwd):/data \
     --volume ~/.azure:/data/.azure \
     --volume /tmp/fake_passwd:/etc/passwd \
-    devopsinfra/docker-terragrunt:azure-tf-1.0.0-tg-0.30.7 terragrunt run-all plan --terragrunt-working-dir ./
+    devopsinfra/docker-terragrunt:azure-tf-${TERRAFORM_VERSION}-tg-${TERRAGRUNT_VERSION} terragrunt run-all plan --terragrunt-working-dir ./
+ 
